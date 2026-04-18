e7ce4c9ccd6daec200b9493c9ca95969cd2421cd
onurerdzhanov23-gif <onurerdzhanov23@gmail.com>
2026-04-18 10:34:07 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('OK');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => console.log(`Puerto ${PORT}`));
